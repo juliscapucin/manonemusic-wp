e9da82f81e2afd08deb4d1c0431b7cfd50543e28
juliscapucin <jscapucin@gmail.com>
2024-04-25 18:45:57 +0200

    init-block-setup: add theme colors

--- app/public/wp-content/themes/manonemusic/single.php
+++ app/public/wp-content/themes/manonemusic/single.php
@@ -1,26 +1,25 @@
 <?php
-  
-  get_header();
 
-  while(have_posts()) {
-    the_post();
-    // pageBanner();
-     ?>
+get_header();
 
-    <div class="container container--narrow page-section">
-          <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/posts'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
-      </div>
+while (have_posts()) {
+  the_post();
+?>
 
-      <h2 class=""><?php the_title(); ?></h2>
-      <div class=""><?php the_content(); ?></div>
-
+  <div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/posts'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
     </div>
-    
 
-    
-  <?php }
+    <h2 class=""><?php the_title(); ?></h2>
+    <div class=""><?php the_content(); ?></div>
 
-  get_footer();
+  </div>
+
+
+
+<?php }
+
+get_footer();
 
 ?>
