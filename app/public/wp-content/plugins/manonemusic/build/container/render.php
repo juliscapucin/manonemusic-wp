d0bd52a48670865c32e068a8a726608cd264dc77
juliscapucin <jscapucin@gmail.com>
2024-05-22 10:28:59 +0200

    release-page: fix container custom block

--- app/public/wp-content/plugins/manonemusic/build/container/render.php
+++ app/public/wp-content/plugins/manonemusic/build/container/render.php
@@ -12,18 +12,40 @@
 
 ?>
 
-<div class='absolute w-full h-full flex justify-center items-center'>
-   <?php
-   foreach ($block->inner_blocks as $inner_block) {
-      $inner_content = $inner_block->inner_content ?? [];
-      $label = $inner_block->attributes['label'] ?? '';
+<?php
 
-      // Check and render image (inner_content) if available
-      if ($inner_block->name === 'core/image' && !empty($inner_content[0])) {
-         $inner_content[0] = str_replace('<figure', '<figure class="relative w-64 aspect-square"', $inner_content[0]);
-         $inner_content[0] = str_replace('<img', '<img class="object-cover w-full h-full"', $inner_content[0]);
-         echo $inner_content[0];
-      }
+// Initialize DOMDocument and load HTML content
+$dom = new DOMDocument();
+libxml_use_internal_errors(true); // Suppress warnings for malformed HTML
+$dom->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+libxml_clear_errors();
+
+// Add the $attributes['classes'] to the element with id 'outer-div'
+$outerDivs = $dom->getElementsByTagName('div');
+foreach ($outerDivs as $outerDiv) {
+   $existingClass = $outerDiv->getAttribute('class');
+   $outerDiv->setAttribute('class', $existingClass . ' ' . $attributes['classes']);
+}
+
+// Add classes to <figure> with id 'image-figure'
+$imageFigures = $dom->getElementsByTagName('figure');
+foreach ($imageFigures as $imageFigure) {
+   $imageFigure->setAttribute('class', 'relative w-64 aspect-square overflow-clip');
+}
+
+// Add classes to <img> tags
+$images = $dom->getElementsByTagName('img');
+if ($images->length > 0) {
+   foreach ($images as $image) {
+      $existingClass = $image->getAttribute('class');
+      $image->setAttribute('class', $existingClass . ' w-full h-full object-cover');
    }
-   ?>
-</div>
+} else {
+   echo "No <img> tags found.";
+}
+
+// Save the modified HTML content
+$content = $dom->saveHTML();
+?>
+
+<?php echo $content; ?>
